d004d29d3b0a98e004ce38c1c1fbfe4fb7ef16e2
M Naccache <monapurrpurr@gmail.com>
2024-08-03 11:52:37 -0700

    Add files via upload
    Added comments

--- RecursivePascal.java
+++ RecursivePascal.java
@@ -1,53 +1,57 @@
-public class RecursivePascal {
+//M NACCACHE
+//Extra credit: I did both lab 6 assignments
+
+public class RecursivePascal { //test class, all in one file because having a file that's 5 lines long is a bit silly
     public static void main (String[] args) {
-        ManagePascal.newCalculate(20);
+        ManagePascal.newCalculate(6); //sending out a Pascal's Triangle that's 6 lines long
+        ManagePascal.newCalculate(20); //sending out a Pascal's Triangle that's 20 lines long
     }
 }
 
 class ManagePascal {
-    public static void newCalculate (int length) {
-        int[] array = {1};
-        calculate(length, 2, array);
+    public static void newCalculate (int length) { //just formats the start of the triangle so the recursion works well
+        length++; //I need to up the length for weird reasons that even I do not understand. This just gives us the right number of rows in the end
+        int[] array = {1}; //makes the first array, it's just the very tip of the triangle
+        calculate(length, 2, array); //here's how tall the triangle needs to be, here's the current row, here's what the last row looks like
     }
 
-    public static void calculate (int length, int count, int[] array) {
-        int[] newArray = new int[count];
+    public static void calculate (int length, int count, int[] array) { //recursivly does each row of the triangle
+        int[] newArray = new int[count]; //a new array to put the calculated values in
 
-        for (int i = 0; i < count - 1; i++) {
+        for (int i = 0; i < count - 1; i++) { //goes through the array and does math into the new array
             newArray[i] = math(array, count, i);
         }
 
-        printTest(length, count, newArray);
-        count++;
-        if (count <= length) {
-            calculate(length, count, newArray);
+        printTest(length, count, newArray); //prints out the Pascal's Triangle row
+        count++; //adds to the count
+        if (count <= length) { //if the count is less than or the same as the length, make a new row
+            calculate(length, count, newArray); //new array becomes array in the newly called method, creating a cycle
         }
     }
 
-    public static int math (int[] array, int count, int number) {
+    public static int math (int[] array, int count, int number) { //this does the actual calculations for the arrays
         int addOne = 0;
-        if (number < 1) {
+        if (number < 1) { //this is so it doesn't throw an error about -1 not being an index in an array
             addOne = 0;
         } else {
             addOne = array[number - 1];
         }
-        //int addOne = array[number - 1];
         int addTwo = array[number];
-        int sum = addOne + addTwo;
+        int sum = addOne + addTwo; //adds the two numbers above it together 
 
         return sum;
     }
 
     public static void printTest (int length, int count, int[] array) {
-        for (int i = 1; i <= length - count; i++) {
+        for (int i = 1; i <= length - count; i++) { //adds some spaces so it gets printed out in a triangle
             System.out.print("   ");
         }
-        String printable = "";
-        for (int j = 0; j < array.length - 1; j++) {
-            printable = array[j] + "            ";
+        String printable; //making the variable outside the for loop so the computer doesn't yell at me
+        for (int j = 0; j < array.length - 1; j++) { //prints out each thing of an array
+            printable = array[j] + "            "; //so it stays at a consistant length
             printable = printable.substring(0, 6);
             System.out.print(printable);
         }
-        System.out.println();
+        System.out.println(); //goes to the next line so it's actually a triangle
     }
 }
